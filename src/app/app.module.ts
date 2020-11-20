ecd6edf217890033cb91434a24a871700dd0f7fe
Andre Vieira da Silva <t_andre@MATRIZ-300680.hapvida.com.br>
2020-11-20 16:53:33 -0300

    init header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header-component/header-component.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './views/footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const routes: Routes = [
+  { path: '', component: HeaderComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
