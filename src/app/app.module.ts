4596422ae3a9f0dc7cac6d3c7e8b3263f2a32227
Andre Vieira da Silva <t_andre@MATRIZ-300680.hapvida.com.br>
2020-11-23 09:05:54 -0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './views/header-component/header-component.component';
-import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FooterComponent } from './views/footer/footer.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-const routes: Routes = [
-  { path: '', component: HeaderComponent }
-];
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    FooterComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
